1bcd98fae234ab50d72d5fb24364c19aebf8184b
Stephanie-Booba <boobastephanie2@gmail.com>
2026-07-21 18:21:21 +0000

    Updated JavaScript Assignment

---
+++ script.js
@@ -0,0 +1,39 @@
+let name = prompt("Enter your name:");
+let age = prompt("Enter your age:");
+let email = prompt("Enter your email:");
+
+alert(`Hi ${name}, your are ${age} years old and we'll keep in touch via your email: ${email}`);
+//Yeterday's work
+console.log("Hello, JavaScript is connected!");
+
+let myName = "Miss Booba";
+console.log(myName);
+
+let myAge = 29;
+console.log(myAge);
+
+let addition = 15 + 10;
+console.log(addition);
+
+let subtraction = 20 - 8;
+console.log(subtraction);
+
+let division = 24 / 6;
+console.log(division);
+
+let multiplication = 7 * 5;
+console.log(multiplication);
+
+let remainder = 15 % 4;
+console.log(remainder);
+
+let isStudent = true;
+console.log(isStudent);
+
+let score = 200
+console.log(typeof score);
+
+let school = "Tech4Girls"
+console.log(typeof school);
+
+
--- style.css/script.js
+++
@@ -1,5 +0,0 @@
-let name = prompt("Enter your name:");
-let age = prompt("Enter your age:");
-let email = prompt("Enter your email:");
-
-alert(`Hi ${name}, your are ${age} years old and we'll keep in touch via your email: ${email}`);
